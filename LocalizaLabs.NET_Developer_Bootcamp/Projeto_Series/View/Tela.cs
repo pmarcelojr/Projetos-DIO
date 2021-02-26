e6eb698aee008790e779b050bdee7bb51b53f694
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-02-26 17:22:40 -0300

    Implementation Menu project_Series

---
+++ LocalizaLabs.NET_Developer_Bootcamp/Projeto_Series/View/Tela.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace Projeto_Series.View
+{
+    public class Tela
+    {
+        public static string Menu()
+        {
+            Console.WriteLine("DIO Seéries a seu dispor !!!");
+            Console.WriteLine();
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1 - Listar séries");
+            Console.WriteLine("2 - Inserir nova série");
+            Console.WriteLine("3 - Atualizar série");
+            Console.WriteLine("4 - Excluir série");
+            Console.WriteLine("5 - Visualizar série");
+            Console.WriteLine("C - Limpar tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcao = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcao;
+        }
+    }
+}
