37de1d07405856e4baeb0d064a0ec32ada753d70
pmarcelojr <marcelosantostecnologia@gmail.com>
2021-02-26 16:56:53 -0300

    Implementation entities in project_Series

---
+++ LocalizaLabs.NET_Developer_Bootcamp/Projeto_Series/Entities/Serie.cs
@@ -0,0 +1,47 @@
+using System;
+using Projeto_Series.Entities.Enum;
+
+namespace Projeto_Series.Entities
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+            Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            return "Gênero: " + Genero + Environment.NewLine +
+                    "Titulo: " + Titulo + Environment.NewLine +
+                    "Descrição: " + Descricao + Environment.NewLine +
+                    "Ano de Inicio: " + Ano;
+        }
+
+        public string RetornaTitulo()
+        {
+            return Titulo;
+        }
+
+        public int RetornaId()
+        {
+            return Id;
+        }
+
+        public void Excluir()
+        {
+            Excluido = true;
+        }
+    }
+}
---
+++ LocalizaLabs.NET_Developer_Bootcamp/Projeto_Series/Entities/SerieRepositorio.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Projeto_Series.Entities.Interfaces;
+
+namespace Projeto_Series.Entities
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+
+        public void Excluir(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
